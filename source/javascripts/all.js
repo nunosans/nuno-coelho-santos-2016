9f0ae3c4ca0bd952bdbeef5c0106c50a727d1234
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-08-08 16:53:30 +0900

    Show less

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,4 +8,10 @@ $('document').ready(function() {
     $('.intro-complete').attr('data-layout', 'visible');
   });
 
+  $('*[data-action="hide-intro-complete"]').click(function(event) {
+    event.preventDefault();
+    $('.intro-short').attr('data-layout', 'visible');
+    $('.intro-complete').attr('data-layout', 'hidden');
+  });
+
 })
