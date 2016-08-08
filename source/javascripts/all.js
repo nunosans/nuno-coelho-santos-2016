47c037d9a0a43bfd8e7fa8ccbf2604db2b7ddcaa
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-08-08 15:37:06 +0900

    Intro read more

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
 // This is where it all goes :)
+
+$('document').ready(function() {
+
+  $('*[data-action="show-intro-complete"]').click(function(event) {
+    event.preventDefault();
+    $('.intro-short').attr('data-layout', 'hidden');
+    $('.intro-complete').attr('data-layout', 'visible');
+  });
+
+})
