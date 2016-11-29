2ffb186c8e2340d9985a9a70ffde1f4fe7a51800
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-11-29 15:39:04 +0900

    Thread desktop

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,10 +2,70 @@
 
 $('document').ready(function() {
 
-  $('*[data-action="show-intro-complete"]').click(function(event) {
+  // Expand introduction text.
+
+  $('button[data-action="show-intro-complete"]').click(function(event) {
     event.preventDefault();
     $('.intro-short').attr('data-layout', 'hidden');
-    $('.intro-complete').attr('data-layout', 'visible');
+    $('.intro-complete, .intro-bio').attr('data-layout', 'visible');
+  });
+
+  // Play and pause Currency for iOS video.
+
+  $('button[data-action="play-currency-iphone"]').click(function(event) {
+    event.preventDefault();
+    $('button[data-action="play-currency-iphone"]').attr('data-layout', 'hidden');
+    $('button[data-action="pause-currency-iphone"]').attr('data-layout', 'visible');
+  });
+
+  $('button[data-action="pause-currency-iphone"]').click(function(event) {
+    event.preventDefault();
+    $('button[data-action="play-currency-iphone"]').attr('data-layout', 'visible');
+    $('button[data-action="pause-currency-iphone"]').attr('data-layout', 'hidden');
+  });
+
+  // Play and pause Thread Browse Mobile video.
+
+  $('button[data-action="play-thread-browse-mobile"]').click(function(event) {
+    event.preventDefault();
+    $('button[data-action="play-thread-browse-mobile"]').attr('data-layout', 'hidden');
+    $('button[data-action="pause-thread-browse-mobile"]').attr('data-layout', 'visible');
+  });
+
+  $('button[data-action="pause-thread-browse-mobile"]').click(function(event) {
+    event.preventDefault();
+    $('button[data-action="play-thread-browse-mobile"]').attr('data-layout', 'visible');
+    $('button[data-action="pause-thread-browse-mobile"]').attr('data-layout', 'hidden');
+  });
+
+  // Play and pause Thread Browse Desktop video.
+
+  $('button[data-action="play-thread-browse-desktop"]').click(function(event) {
+    event.preventDefault();
+    $('button[data-action="play-thread-browse-desktop"]').attr('data-layout', 'hidden');
+    $('button[data-action="pause-thread-browse-desktop"]').attr('data-layout', 'visible');
+  });
+
+  $('button[data-action="pause-thread-browse-desktop"]').click(function(event) {
+    event.preventDefault();
+    $('button[data-action="play-thread-browse-desktop"]').attr('data-layout', 'visible');
+    $('button[data-action="pause-thread-browse-desktop"]').attr('data-layout', 'hidden');
+  });
+
+  // Hide and show columns.
+
+  $('button[data-action="show-columns"]').click(function(event) {
+    event.preventDefault();
+    $('*[data-action="show-columns"]').attr('data-layout', 'hidden');
+    $('*[data-action="hide-columns"]').attr('data-layout', 'visible');
+    $('#column-grid').attr('data-layout', 'visible');
+  });
+
+  $('button[data-action="hide-columns"]').click(function(event) {
+    event.preventDefault();
+    $('*[data-action="show-columns"]').attr('data-layout', 'visible');
+    $('*[data-action="hide-columns"]').attr('data-layout', 'hidden');
+    $('#column-grid').attr('data-layout', 'hidden');
   });
 
 })
